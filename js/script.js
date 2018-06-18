7e10b27f9d154189484a153e6552a215638e4262
Angela Norris <cocoanorris@yahoo.com>
2018-06-18 16:39:38 -0400

    add functionality of starting and stopping gifs on click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 // Initial array of gifs
-var instruments = ["Piano", "Banjo", "Flute", "Trumpet","Saxaphone", "Clarinet", "Trombone", "Violin", "Snare Drum", "Guitar"];
+var instruments = ["Piano", "Banjo", "Harmonica", "Trumpet","Saxaphone", "Clarinet", "Trombone", "Violin", "Snare Drum", "Guitar"];
 
 // displayGigInfo function re-renders the HTML to display the appropriate content
 function displayGifInfo() {
@@ -15,8 +15,8 @@ function displayGifInfo() {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    //console.log(response);
-
+    console.log(response);
+   
     for (var i = 0; i < response.data.length; i++) {
     // Creating a div to hold the gif
     var instrumentDiv = $("<div class='instrument'>");
@@ -32,35 +32,57 @@ function displayGifInfo() {
     instrumentDiv.prepend(pOne);
     
     //This is storing url for an image
-    var gifUrl = response.data[i].images.original.url;
-
+    var gifUrl = response.data[i].images.fixed_height_still.url;
+    //console.log(response.data[i].images.original.url)
+    var gifUrlAnimate = response.data[i].images.original.url;
     //This is storing and creating an img element
     var gifImage = $("<img>");
 
     //These are adding a src attribute of the orgininal url and an alt attribute to our new img element
     gifImage.attr("src", gifUrl);
-    gifImage.attr("alt", "instrument image");
+    gifImage.attr("data-still", gifUrl);
+    gifImage.attr("data-animate", gifUrlAnimate);
+    gifImage.attr("data-state", "still");
+    gifImage.addClass("gif");
+    //gifImage.attr("alt", "instrument image");
     //console.log(gifImage);
 
     //Appending gif
     instrumentDiv.append(gifImage);
 
     $("#gif-view").append(instrumentDiv);
-    }
+
     
+
+    }
+    $(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
   });
 
 }
 
+
+
 // Function for displaying movie data
 function renderButtons() {
 
-  // Deleting the movies prior to adding new movies
  
   // (this is necessary otherwise you will have repeat buttons)
   $("#buttons-view").empty();
 
-  // Looping through the array of movies
+  // Looping through the array of gifs
   for (var i = 0; i < instruments.length; i++) {
 
     // Then dynamicaly generating buttons for each gif in the array
